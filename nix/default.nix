cb56ade9cda7dd72654968a48976372120e9aaca
Sixels <sixels@protonmail.com>
2022-11-16 15:35:12 -0300

    Simplify crate configuration

--- nix/default.nix
+++ nix/default.nix
@@ -5,32 +5,4 @@ let cargoToml = (builtins.fromTOML (builtins.readFile ../Cargo.toml));
 
 in naersk.lib."${targetPlatform.system}".buildPackage rec {
   src = ./.;
-
-  buildInputs = [ rustfmt pkg-config cargo rustc libiconv clippy ];
-  checkInputs = [ cargo rustc ];
-
-  doCheck = true;
-  # CARGO_BUILD_INCREMENTAL = "false";
-  # RUST_BACKTRACE = "full";
-  copyLibs = true;
-
-  # Optional things you might need:
-  #
-  # If you depend on `libclang`:
-  # LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
-  #
-  # If you depend on protobuf:
-  # PROTOC = "${protobuf}/bin/protoc";
-  # PROTOC_INCLUDE = "${protobuf}/include";
-
-  name = cargoToml.package.name;
-  version = cargoToml.package.version;
-
-  meta = with lib; {
-    description = cargoToml.package.description;
-    homepage = cargoToml.package.homepage;
-    license = with licenses; [ mit ];
-    maintainers = with maintainers; [ ];
-    mainProgram = "wani_clone";
-  };
 }
