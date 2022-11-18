e14dab4a15234f181f8ea688debf6678b7e510b9
Sixels <sixels@protonmail.com>
2022-11-18 16:11:57 -0300

    Create utils migration

---
+++ migrations/00000000000000_utils.up.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
