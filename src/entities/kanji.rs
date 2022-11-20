6ba761788bbcd93f4df1be6f57f1d27e60450a20
Sixels <sixels@protonmail.com>
2022-11-20 16:23:57 -0300

    Add helper functions for testing

--- src/entities/kanji.rs
+++ src/entities/kanji.rs
@@ -59,3 +59,44 @@ pub struct InsertKanji {
     pub reading_mnemonic: String,
     pub radical_composition: Vec<String>,
 }
+
+// #[cfg(test)]
+// pub(crate) fn kanji_genius() -> InsertKanji {
+//     InsertKanji::builder()
+//         .name("genius")
+//         .symbol("才")
+//         .reading("さい")
+//         .onyomi(vec!["さい".to_owned()])
+//         .meaning_mnemonic(r#"On the ground you put barbs at the bottom of a slide because you're a genius trying to catch another genius."#)
+//         .reading_mnemonic(r#"You check your genius trap, and there's a genius stuck in the barbs! You know it's a genius because they're actually a cyborg (さい)."#)
+//         .radical_composition(vec!["ground".into(),"barb".into(), "slide".into()])
+//         .build()
+// }
+
+#[cfg(test)]
+pub(crate) fn kanji_middle() -> InsertKanji {
+    InsertKanji::builder()
+        .name("middle")
+        .symbol("中")
+        .reading("ちゅう")
+        .onyomi(vec!["ちゅう".to_owned()])
+        .kunyomi(vec!["なか".to_owned()])
+        .meaning_mnemonic(r#"The radical Middle and the kanji Middle are both the same. So if you know one, you know the other."#)
+        .reading_mnemonic(r#"To remember the reading for this kanji, we use the word Chewbacca to pull up ちゅう in our memory. If you remember back to the radical 中, the middle of your mouth was stabbed with a stick. You look up to see who did it. There stands Chewbacca, doing his Chewbacca yell. And it isn't a stick in your mouth, it's an arrow from his crossbow (a bowcaster, actually). It just so happens Chewbacca's bowcaster looks just like this kanji too. Go figure."#)
+        .radical_composition(vec!["middle".into()])
+        .build()
+}
+
+#[cfg(test)]
+pub(crate) fn kanji_stop() -> InsertKanji {
+    InsertKanji::builder()
+        .name("stop")
+        .symbol("止")
+        .reading("し")
+        .onyomi(vec!["し".to_owned()])
+        .kunyomi(vec!["と".to_owned(), "や".to_owned()])
+        .meaning_mnemonic(r#"The stop radical is the same as the stop kanji."#)
+        .reading_mnemonic(r#"You have to stop because there is a sheep (し) in front of you. You try to walk around the sheep, but it moves to stop in front of you again. Every time the sheep stops you stop too."#)
+        .radical_composition(vec!["stop".into()])
+        .build()
+}
--- src/entities/radical.rs
+++ src/entities/radical.rs
@@ -42,3 +42,48 @@ impl InsertRadical {
         }
     }
 }
+
+// #[cfg(test)]
+// pub(crate) fn radical_ground() -> InsertRadical {
+//     InsertRadical::builder()
+//         .name("ground")
+//         .symbol("一")
+//         .meaning_mnemonic("This radical consists of a single, horizontal stroke. What's the biggest, single, horizontal stroke? That's the ground. Look at the ground, look at this radical, now look at the ground again. Kind of the same, right?")
+//         .build()
+// }
+
+#[cfg(test)]
+pub(crate) fn radical_barb() -> InsertRadical {
+    InsertRadical::builder()
+        .name("barb")
+        .symbol("亅")
+        .meaning_mnemonic(r#"This radical is shaped like a barb. Like the kind you'd see on barb wire. Imagine one of these getting stuck to your arm or your clothes. Think about how much it would hurt with that little hook on the end sticking into you. Say out loud, "Oh dang, I got a barb stuck in me!""#)
+        .build()
+}
+
+// #[cfg(test)]
+// pub(crate) fn radical_slide() -> InsertRadical {
+//     InsertRadical::builder()
+//         .name("slide")
+//         .symbol("丿")
+//         .meaning_mnemonic("Close your eyes and imagine you're a kid again. Now open them and... look! It's a slide! Imagine little you sliding down this slide over and over, having a great time. Imagination + emotion makes for good memorization!")
+//         .build()
+// }
+
+#[cfg(test)]
+pub(crate) fn radical_middle() -> InsertRadical {
+    InsertRadical::builder()
+        .name("middle")
+        .symbol("中")
+        .meaning_mnemonic("There's a stick going right through the middle of someone's mouth! Imagine that mouth being yours. Measure the placement of the stick. Perfectly aligned, right in the middle. That's amazing, though to be honest, you probably have bigger problems than measuring the location of this stick in your mouth.")
+        .build()
+}
+
+#[cfg(test)]
+pub(crate) fn radical_stop() -> InsertRadical {
+    InsertRadical::builder()
+        .name("stop")
+        .symbol("止")
+        .meaning_mnemonic("There's a giant toe and a stick in the ground in front of you. You're driving your car toward them, but you don't see them until your lights hit them. What would these things cause you to do? Most likely stop your car right away.")
+        .build()
+}
