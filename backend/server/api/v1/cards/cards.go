cbec9a4ed23e0a8a4a2e3dd42bfb94e320152248
Sixels <sixels@protonmail.com>
2023-01-08 21:26:48 -0300

    Require authentication for some cards operations

--- backend/server/api/v1/cards/cards.go
+++ backend/server/api/v1/cards/cards.go
@@ -1,6 +1,8 @@
 package v1
 
 import (
+	"sixels.io/manekani/server/middlewares"
+	"sixels.io/manekani/services/auth"
 	"sixels.io/manekani/services/cards"
 	"sixels.io/manekani/services/files"
 
@@ -8,39 +10,45 @@ import (
 )
 
 type CardsApi struct {
-	cards *cards.CardsService
-	files *files.FilesService
+	cards         *cards.CardsService
+	files         *files.FilesService
+	authenticator *auth.Authenticator
 }
 
-func New(cardsService *cards.CardsService, filesService *files.FilesService) *CardsApi {
+func New(
+	cardsService *cards.CardsService,
+	filesService *files.FilesService,
+	authService *auth.Authenticator,
+) *CardsApi {
 	return &CardsApi{
-		cards: cardsService,
-		files: filesService,
+		cards:         cardsService,
+		files:         filesService,
+		authenticator: authService,
 	}
 }
 
 func (api *CardsApi) SetupRoutes(router *echo.Echo) {
 	apiV1 := router.Group("/api/v1")
 
-	apiV1.POST("/radical", api.CreateRadical, api.UploadRadicalImage)
+	apiV1.POST("/radical", api.CreateRadical, middlewares.LoginRequired(*api.authenticator), api.UploadRadicalImage)
 	apiV1.GET("/radical/:name", api.QueryRadical)
-	apiV1.PATCH("/radical/:name", api.UpdateRadical)
-	apiV1.DELETE("/radical/:name", api.DeleteRadical)
+	apiV1.PATCH("/radical/:name", api.UpdateRadical, middlewares.LoginRequired(*api.authenticator))
+	apiV1.DELETE("/radical/:name", api.DeleteRadical, middlewares.LoginRequired(*api.authenticator))
 	apiV1.GET("/radical", api.AllRadicals)
 	apiV1.GET("/radical/:name/kanji", api.QueryRadicalKanjis)
 
-	apiV1.POST("/kanji", api.CreateKanji)
+	apiV1.POST("/kanji", api.CreateKanji, middlewares.LoginRequired(*api.authenticator))
 	apiV1.GET("/kanji/:symbol", api.QueryKanji)
-	apiV1.PATCH("/kanji/:symbol", api.UpdateKanji)
-	apiV1.DELETE("/kanji/:symbol", api.DeleteKanji)
+	apiV1.PATCH("/kanji/:symbol", api.UpdateKanji, middlewares.LoginRequired(*api.authenticator))
+	apiV1.DELETE("/kanji/:symbol", api.DeleteKanji, middlewares.LoginRequired(*api.authenticator))
 	apiV1.GET("/kanji", api.AllKanji)
 	apiV1.GET("/kanji/:symbol/radicals", api.QueryKanjiRadicals)
 	apiV1.GET("/kanji/:symbol/vocabularies", api.QueryKanjiVocabularies)
 
-	apiV1.POST("/vocabulary", api.CreateVocabulary)
+	apiV1.POST("/vocabulary", api.CreateVocabulary, middlewares.LoginRequired(*api.authenticator))
 	apiV1.GET("/vocabulary/:word", api.QueryVocabulary)
-	apiV1.PATCH("/vocabulary/:word", api.UpdateVocabulary)
-	apiV1.DELETE("/vocabulary/:word", api.DeleteVocabulary)
+	apiV1.PATCH("/vocabulary/:word", api.UpdateVocabulary, middlewares.LoginRequired(*api.authenticator))
+	apiV1.DELETE("/vocabulary/:word", api.DeleteVocabulary, middlewares.LoginRequired(*api.authenticator))
 	apiV1.GET("/vocabulary", api.AllVocabularies)
 	apiV1.GET("/vocabulary/:word/kanji", api.QueryVocabularyKanjis)
 
