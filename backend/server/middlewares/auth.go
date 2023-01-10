0f69a2bd8e641f8b04b69a76724168c674bd1108
Sixels <sixels@protonmail.com>
2023-01-10 18:31:46 -0300

    Store oauth2 token directly into the session instead of a wrapper type

--- backend/server/middlewares/auth.go
+++ backend/server/middlewares/auth.go
@@ -5,13 +5,14 @@ import (
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
 	"sixels.io/manekani/services/auth"
 )
 
 func LoginRequired(authenticator auth.Authenticator) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session := sessions.DefaultMany(c, "user-session")
-		if token, ok := session.Get("AuthToken").(auth.StaticToken); !ok || !auth.ReviveToken(token).Valid() {
+		if token, ok := session.Get("AuthToken").(oauth2.Token); !ok || !token.Valid() {
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
