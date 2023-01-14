0322e2dfa01d1effc71d8e8a14e860d6d71709be
Sixels <sixels@protonmail.com>
2023-01-14 11:56:52 -0300

    Add some helpful logs

--- backend/server/api/v1/cards/middleware.go
+++ backend/server/api/v1/cards/middleware.go
@@ -17,17 +17,18 @@ import (
 func (api *CardsApi) UploadRadicalImage() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		radicalName := strings.TrimSpace(c.Request.FormValue("name"))
+		log.Println("uploading radical image", radicalName)
 
 		// upload image if any
 		radicalSymbolImage, err := c.FormFile("symbol_image")
 		if err == nil && radicalName != "" {
-			log.Println("middleware: ", radicalName)
 			radicalSymbolFile, err := radicalSymbolImage.Open()
 			if err != nil {
 				c.AbortWithError(http.StatusBadRequest, errors.InvalidRequest("invalid file in symbol_image"))
 				return
 			}
 			defer radicalSymbolFile.Close()
+			log.Println("image size:", radicalSymbolImage.Size)
 
 			objectKey, err := uploadFile(c.Request.Context(), api.files, radicalSymbolFile, files.FileInfo{
 				Size:      radicalSymbolImage.Size,
@@ -36,13 +37,20 @@ func (api *CardsApi) UploadRadicalImage() gin.HandlerFunc {
 				Kind:      "image",
 			})
 			if err != nil {
+				log.Println("failed")
+				log.Println(err)
 				c.AbortWithError(err.(*errors.Error).Status, err)
 				return
 			}
 
 			form := c.Request.Form
+
 			form.Set("symbol", objectKey)
 			form.Del("symbol_image")
+			c.Request.MultipartForm.Value["symbol"] = []string{objectKey}
+			c.Request.MultipartForm.Value["symbol_image"] = []string{}
+
+			log.Println("image uploaded")
 		}
 
 		c.Next()
