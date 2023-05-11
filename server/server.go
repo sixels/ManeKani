9c17a47c95887fc909e1d3afee7ba482d0446e43
Sixels <sixels@protonmail.com>
2023-05-11 16:25:35 -0300

    Register new APIs to the server

--- server/server.go
+++ server/server.go
@@ -8,24 +8,23 @@ import (
 	"os"
 	"os/user"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/supertokens/supertokens-golang/supertokens"
 
-	"github.com/sixels/manekani/core/services/cards"
+	"github.com/sixels/manekani/core/adapters/cards"
+	"github.com/sixels/manekani/core/adapters/tokens"
 	_ "github.com/sixels/manekani/docs/manekani"
 
-	auth_api "github.com/sixels/manekani/server/api/auth"
 	cards_api "github.com/sixels/manekani/server/api/cards"
 	files_api "github.com/sixels/manekani/server/api/files"
+	tokens_api "github.com/sixels/manekani/server/api/tokens"
 	users_api "github.com/sixels/manekani/server/api/users"
+	auth_api "github.com/sixels/manekani/server/auth"
 
-	"github.com/sixels/manekani/services/auth"
 	"github.com/sixels/manekani/services/ent"
 	cards_repo "github.com/sixels/manekani/services/ent/cards"
+	"github.com/sixels/manekani/services/ent/token"
 	"github.com/sixels/manekani/services/ent/users"
 	"github.com/sixels/manekani/services/files"
-	"github.com/sixels/manekani/services/jwt"
 )
 
 type Service interface {
@@ -41,19 +40,18 @@ type Server struct {
 func New() *Server {
 	gob.Register(user.User{})
 
-	jwtService := jwt.CreateService(os.Getenv("TOKEN_SIGN_KEY"))
 	entRepo, err := ent.NewRepository()
 	if err != nil {
 		log.Fatalf("Could not connect with ManeKani database: %v", err)
 	}
+
+	tokensService := tokens.CreateAdapter(token.NewRepository(entRepo))
+	authService := auth_api.NewAuthService(&tokensService)
+
 	cardsRepo := cards_repo.NewRepository(entRepo)
-	usersRepo, err := users.NewRepository(context.Background(), entRepo, jwtService)
+	usersRepo := users.NewRepository(entRepo)
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	if err := auth.StartAuthenticator(usersRepo); err != nil {
-		log.Fatalf("Could setup the 'user' repository: %v", err)
 	}
 
 	filesRepo, err := files.NewRepository(context.Background())
@@ -61,39 +59,40 @@ func New() *Server {
 		log.Fatalf("Could not setup the 'file' repository: %v", err)
 	}
 
-	cardsV1 := cards_api.New(cards.NewService(cardsRepo, filesRepo), filesRepo, usersRepo, jwtService)
+	tokensAPI := tokens_api.New(&tokensService, authService)
+	cardsV1 := cards_api.New(cards.CreateAdapter(cardsRepo, filesRepo), filesRepo, usersRepo, authService)
 	filesAPI := files_api.New(filesRepo)
-	usersAPI := users_api.New(usersRepo, jwtService)
+	usersAPI := users_api.New(usersRepo, authService)
 
 	router := gin.Default()
 
 	return &Server{
 		router:   router,
-		services: []Service{cardsV1, filesAPI, usersAPI},
+		services: []Service{cardsV1, filesAPI, usersAPI, tokensAPI, authService},
 	}
 }
 
 func (server *Server) Start(logFile io.Writer) {
-	clientURL := os.Getenv("MANEKANI_CLIENT_URL")
-	clientPort := os.Getenv("MANEKANI_CLIENT_PORT")
+	// clientURL := os.Getenv("MANEKANI_CLIENT_URL")
+	// clientPort := os.Getenv("MANEKANI_CLIENT_PORT")
 	serverPort := os.Getenv("MANEKANI_SERVER_PORT")
 
-	trustedOrigins := []string{
-		clientURL + ":" + clientPort,
-		"http://localhost:" + clientPort,
-	}
+	// trustedOrigins := []string{
+	// 	clientURL + ":" + clientPort,
+	// 	"http://localhost:" + clientPort,
+	// }
 
-	corsConfig := cors.Config{
-		AllowCredentials: true,
-		AllowOrigins:     trustedOrigins,
-		AllowMethods:     []string{"GET", "POST", "DELETE", "PUT", "OPTIONS", "PATCH"},
-		AllowHeaders: append([]string{"Content-Type"},
-			supertokens.GetAllCORSHeaders()...),
-	}
-	server.router.Use(cors.New(corsConfig))
+	// corsConfig := cors.Config{
+	// 	AllowCredentials: true,
+	// 	AllowOrigins:     trustedOrigins,
+	// 	AllowMethods:     []string{"GET", "POST", "DELETE", "PUT", "OPTIONS", "PATCH"},
+	// 	AllowHeaders: append([]string{"Content-Type"},
+	// 		supertokens.GetAllCORSHeaders()...),
+	// }
+	// server.router.Use(cors.New(corsConfig))
 
-	// SuperTokens
-	server.router.Use(auth_api.SupertokensMiddleware)
+	// // SuperTokens
+	// server.router.Use(auth_api.SupertokensMiddleware)
 
 	server.bindRoutes()
 
